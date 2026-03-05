569a1585ffecaa581f04835a00a3f51bc706dcbf
Juan <jpablonunesvinas@gmail.com>
2026-03-05 19:11:26 +0000

    update hello

--- hello.py
+++ hello.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada
+ no ambiente o programa exibe a
+ mensagem correspondente.
+
+como usar:
+
+tenha a Variavel LANG devidamente configurada ex: 
+
+  export LANG=pt_BR
+
+Excução:
+
+  pytthon3 hello.py
+  ou 
+  ./hello.py
+     
+"""
+__version__ = "0.0.1"
+__author__ = "juan vinas"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!" 
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"    
+
 # teste de comentario 1
-print("Hello, world!")
+print(msg)
+
 
 
 
